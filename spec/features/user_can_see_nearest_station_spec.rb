20298a3e69bf7536ba3571dfc0dfb3e9613a6019
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-04-10 11:06:23 -0600

    add direction tests

--- spec/features/user_can_see_nearest_station_spec.rb
+++ spec/features/user_can_see_nearest_station_spec.rb
@@ -11,6 +11,10 @@ RSpec.describe 'As a user' do
       expect(page).to have_content("Address: 1225 17th St.")
       expect(page).to have_content("Fuel type: ELEC")
       expect(page).to have_content("Access times: MO: Not Specified; TU: Not Specified; WE: Not Specified; TH: Not Specified; FR: Not Specified; SA: Not Specified; SU: Not Specified")
+      expect(page).to have_content("Distance: 0.1 m")
+      expect(page).to have_content("Travel time: 1 min")
+      expect(page).to have_content("Directions: ")
+      save_and_open_page
     end
 
   end
